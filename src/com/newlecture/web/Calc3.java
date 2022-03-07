69668d32189f7550663d3794bde9d8fbd6105ec8
DESKTOP-R6E7F3N\aromit <sde3300@naver.com>
2022-03-07 13:27:18 +0900

    36강. 계산기 서블릿 완성하기

--- src/com/newlecture/web/Calc3.java
+++ src/com/newlecture/web/Calc3.java
@@ -2,14 +2,15 @@ package com.newlecture.web;
 
 import java.io.IOException;
 
-import javax.servlet.ServletContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @WebServlet("/calc3")
 public class Calc3 extends HttpServlet {
@@ -17,84 +18,42 @@ public class Calc3 extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		// 어플리케이션 저장소 : ServletContext
-		ServletContext application = request.getServletContext();
-		HttpSession session = request.getSession();
 		Cookie[] cookies = request.getCookies(); // 쿠키를 읽을 때 request 사용
 
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html; charset=UTF-8");
-
-		String v_ = request.getParameter("v");
-		String op = request.getParameter("operator");
-
-		int v = 0;
-
-		if (!v_.equals(""))
-			v = Integer.parseInt(v_);
-
-		// 계산을 위한 조건 설정
-		if (op.equals("=")) {
-
-			// x = 담은 값을 가져와 x에 저장(꺼내서 사용)
-			// int x = (Integer)application.getAttribute("value");
-			// int x = (Integer)session.getAttribute("value");
-
-			int x = 0;
-			for (Cookie c : cookies) {
-				if (c.getName().equals("value")) {
-					x = Integer.parseInt(c.getValue());
+		// 사용자가 입력한 내용
+		String value = request.getParameter("value");
+		String operator = request.getParameter("operator");
+		String dot = request.getParameter("dot");
+		
+		// 쿠키에서 읽어와서 사용자가 입력한 내용 덧붙이는 작업.
+		String exp = "";
+		if(cookies != null) 
+			for(Cookie c : cookies)
+				if(c.getName().equals("exp")) {
+					exp = c.getValue();
 					break;
 				}
+		
+		if(operator != null && operator.equals("=")) {
+			ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+			try {
+				exp = String.valueOf(engine.eval(exp));
+			} catch (ScriptException e) {
+				e.printStackTrace();
 			}
-
-			int y = v;
-			// operator = 담은 값을 가져와 operator에 저장(꺼내서 사용)
-			// String operator = (String)application.getAttribute("op");
-			// String operator = (String) session.getAttribute("op");
-
-			String operator = "";
-			for (Cookie c : cookies) {
-				if (c.getName().equals("op")) {
-					operator = c.getValue();
-					break;
-				}
-			}
-
-			int result = 0;
-
-			if (operator.equals("+")) {
-				result = x + y;
-			} else {
-				result = x - y;
-			}
-
-			response.getWriter().printf("result is %d\n", result);
+		} else {
+			// 쿠키로 저장하는 작업.
+			// exp에 누적 진행(아래 중 null이 아닌 값 1가지만 오게됨)
+			exp += (value == null)?"":value; // value가 null이면 ""빈문자열, 그렇지 않다면 value값을. 
+			exp += (operator == null)?"":operator;
+			exp += (dot == null)?"":dot;
 		}
-		// 저장을 위한 조건 설정
-		// 오퍼레이터 입력이 덧셈 또는 뺄셈인 경우, 그냥 값 저장만 진행
-		else {
-			// application.setAttribute("value", v);
-			// application.setAttribute("op", op);
-
-			// session.setAttribute("value", v);
-			// session.setAttribute("op", op);
-
-			Cookie valueCookie = new Cookie("value", String.valueOf(v));
-			Cookie opCookie = new Cookie("op", op);
-			//valueCookie.setPath("/");   // 모든 경로 
-			//opCookie.setPath("/");      // 모든 경로
-			//valueCookie.setPath("/add");  // add라는 url 요청 시
-			//opCookie.setPath("/add");     // add라는 url 요청 시
-			valueCookie.setPath("/calc2");  // calc2라는 url 요청 시
-			opCookie.setPath("/calc2");     // calc2라는 url 요청 시
-			valueCookie.setMaxAge(24*60*60);	// 만료날짜 지정 (초단위로 지정, 60은 1분)
-						
-			response.addCookie(valueCookie);
-			response.addCookie(opCookie);
-			response.sendRedirect("calc2.html"); // redirect 사용 경로 우회
-		}
-
+		
+		
+		Cookie expCookie = new Cookie("exp", exp);
+		
+		response.addCookie(expCookie);		
+		response.sendRedirect("calcpage"); // redirect 사용 경로 우회
 	}
 
 }
--- src/com/newlecture/web/CalcPage.java
+++ src/com/newlecture/web/CalcPage.java
@@ -5,19 +5,32 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/calcPage")
+@WebServlet("/calcpage")
 public class CalcPage extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
+		Cookie[] cookies = request.getCookies();
+		
+		String exp = "0";
+		if(cookies != null) 
+			for(Cookie c : cookies)
+				if(c.getName().equals("exp")) {
+					exp = c.getValue();
+					break;
+				}
+		
+		
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
+		
 		
 		out.write("<!DOCTYPE html>");
 		out.write("<html>");
@@ -44,35 +57,35 @@ public class CalcPage extends HttpServlet {
 		out.write("	<form action=\"calc3\" method=\"post\">");
 		out.write("		<table>");
 		out.write("			<tr>");
-		out.printf("				<td class=\"output\" colspan=\"4\">%d</td>", 3+4);
+		out.printf("				<td class=\"output\" colspan=\"4\">%s</td>", exp);
 		out.write("			</tr>");
 		out.write("			<tr>");
 		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"CE\" /></td>");
 		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"C\" /></td>");
 		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"BS\" /></td>");
-		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"÷\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"/\" /></td>");
 		out.write("			</tr>");
 		out.write("			<tr>");
-		out.write("				<td><input type=\"submit\" name=\"vlaue\" value=\"7\" /></td>");
-		out.write("				<td><input type=\"submit\" name=\"vlaue\" value=\"8\" /></td>");
-		out.write("				<td><input type=\"submit\" name=\"vlaue\" value=\"9\" /></td>");
-		out.write("				<td><input type=\"submit\" name=\"vlaue\" value=\"X\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"7\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"8\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"9\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"*\" /></td>");
 		out.write("			</tr>");
 		out.write("			<tr>");
-		out.write("				<td><input type=\"submit\" name=\"vlaue\" value=\"4\" /></td>");
-		out.write("				<td><input type=\"submit\" name=\"vlaue\" value=\"5\" /></td>");
-		out.write("				<td><input type=\"submit\" name=\"vlaue\" value=\"6\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"4\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"5\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"6\" /></td>");
 		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"-\" /></td>");
 		out.write("			</tr>");
 		out.write("			<tr>");
-		out.write("				<td><input type=\"submit\" name=\"vlaue\" value=\"1\" /></td>");
-		out.write("				<td><input type=\"submit\" name=\"vlaue\" value=\"2\" /></td>");
-		out.write("				<td><input type=\"submit\" name=\"vlaue\" value=\"3\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"1\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"2\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"3\" /></td>");
 		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"+\" /></td>");
 		out.write("			</tr>");
 		out.write("			<tr>");
 		out.write("				<td></td>");
-		out.write("				<td><input type=\"submit\" name=\"vlaue\" value=\"0\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"0\" /></td>");
 		out.write("				<td><input type=\"submit\" name=\"dot\" value=\".\" /></td>");
 		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"=\" /></td>");
 		out.write("			</tr>");
