b1a51d8396a67a72b18453177359dfb3d4923930
DESKTOP-R6E7F3N\aromit <sde3300@naver.com>
2022-03-07 15:58:50 +0900

    40강. JSP 시작하기

--- src/com/newlecture/web/Calculator.java
+++ src/com/newlecture/web/Calculator.java
@@ -1,36 +1,151 @@
 package com.newlecture.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/calculator")
 public class Calculator extends HttpServlet{
+	
+	// GET 요청만 따로 처리 할 경우
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) 
-							throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		if(req.getMethod().equals("GET")) {       // req.getMethod는 요청온 문자열을 반환
-			System.out.println("GET 요청이 왔습니다.");
-		}
-		else if(req.getMethod().equals("POST")) {
-			System.out.println("POST 요청이 왔습니다.");
-		}
-		super.service(req, resp);
+		Cookie[] cookies = request.getCookies();
+		
+		String exp = "0";
+		if(cookies != null) 
+			for(Cookie c : cookies)
+				if(c.getName().equals("exp")) {
+					exp = c.getValue();
+					break;
+				}
+		
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		out.write("<!DOCTYPE html>");
+		out.write("<html>");
+		out.write("<head>");
+		out.write("<meta charset=\"UTF-8\">");
+		out.write("<title>Insert title here</title>");
+		out.write("<style>");
+		out.write("input {");
+		out.write("width: 50px;");
+		out.write("height: 50px;");
+		out.write("}");
+
+		out.write(".output {");
+		out.write("height: 50px;");
+		out.write("background: #e9e9e9;");
+		out.write("font-size: 24px;");
+		out.write("font-weight: bold;");
+		out.write("text-align: right;");
+		out.write("padding: 0px, 5px;");
+		out.write("}");
+		out.write("</style>");
+		out.write("</head>");
+		out.write("<body>");
+		out.write("	<form method=\"post\">");
+		out.write("		<table>");
+		out.write("			<tr>");
+		out.printf("				<td class=\"output\" colspan=\"4\">%s</td>", exp);
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"CE\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"C\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"BS\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"/\" /></td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"7\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"8\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"9\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"*\" /></td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"4\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"5\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"6\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"-\" /></td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"1\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"2\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"3\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"+\" /></td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"0\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"dot\" value=\".\" /></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"=\" /></td>");
+		out.write("			</tr>");
+		out.write("		</table>");
+		out.write("	</form>");
+		out.write("</body>");
+		out.write("</html>");
 	}
 	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("doGET 메소드가 호출 되었습니다.");
-	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("doPOST 메소드가 호출 되었습니다.");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Cookie[] cookies = request.getCookies(); // 쿠키를 읽을 때 request 사용
+
+		// 사용자가 입력한 내용
+		String value = request.getParameter("value");
+		String operator = request.getParameter("operator");
+		String dot = request.getParameter("dot");
+		
+		// 쿠키에서 읽어와서 사용자가 입력한 내용 덧붙이는 작업.
+		String exp = "";
+		if(cookies != null) 
+			for(Cookie c : cookies)
+				if(c.getName().equals("exp")) {
+					exp = c.getValue();
+					break;
+				}
+		
+		
+		if(operator != null && operator.equals("=")) {
+			ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+			try {
+				exp = String.valueOf(engine.eval(exp));
+			} catch (ScriptException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+          
+          // 쿠키 삭제 조건.
+		} else if(operator != null && operator.equals("C")) {
+			exp = "";
+            
+		} else {
+			// 쿠키로 저장하는 작업.
+			// exp에 누적 진행(아래 중 null이 아닌 값 1가지만 오게됨)
+			exp += (value == null)?"":value; // value가 null이면 ""빈문자열, 그렇지 않다면 value값을. 
+			exp += (operator == null)?"":operator;
+			exp += (dot == null)?"":dot;
+		}
+		
+		
+		Cookie expCookie = new Cookie("exp", exp);
+        // 쿠키 삭제
+		if(operator != null && operator.equals("C")) {
+			expCookie.setMaxAge(0);		
+		}
+		expCookie.setPath("/calculator"); // calculator로만 쿠키 전달, 다른 url에는 전달안된다.
+		response.addCookie(expCookie);		
+		response.sendRedirect("calculator"); // redirect 사용 경로 우회
 	}
 
 }
